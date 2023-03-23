97b461b1de529cb74f026a6130d79aa6f3223c75
Nikola Knezevic <knezevic@etf.bg.ac.rs>
2023-03-23 09:03:47 -0700

    Comments added to service script

--- Kodovi/hello_world/src/hello_world_service.py
+++ Kodovi/hello_world/src/hello_world_service.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import rospy
-from hello_world.srv import add_value_file, add_value_fileResponse
+from hello_world.srv import add_value_file, add_value_fileResponse              # Load service 
 
-def respons_callback(req):
-    f = open('value_file.txt', 'a+')
-    f.write(str(req.value)+'\n')
-    f.close()
-    return add_value_fileResponse(True)
+def respons_callback(req):                              # Create respons callback function
+    f = open('value_file.txt', 'a+')                    # Open file "value_file.txt" in append mode at the end of file
+    f.write(str(req.value)+'\n')                        # Cast int64 in string type add newline character and write it in file
+    f.close()                                           # Close file
+    return add_value_fileResponse(True)                 # Send repsonse to caller
 
-rospy.init_node('add_value_file_service')
-srv = rospy.Service('add_value_file', add_value_file, respons_callback)
-rospy.loginfo('Service is ready!!!')
-rospy.spin()
+rospy.init_node('add_value_file_service')                                           # Init node
+srv = rospy.Service('add_value_file', add_value_file, respons_callback)             # Create service
+rospy.loginfo('Service is ready!!!')                                                # Tell user that the service is ready
+rospy.spin()                                                                        # Keep the code runing
